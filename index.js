8d7cf0d8dd04b3ec2d54cd06230f411921d659be
ahsan <zaman2ahsan@gmail.com>
2022-04-10 01:06:38 +0600

    date input and return per token USD values

--- index.js
+++ index.js
@@ -1,49 +1,111 @@
+const args = require('yargs').argv;
+const axios = require('axios')
 
-    var output = [];
+var getPortfolioValPerToken = (date) => {
+    return new Promise(function (resolve) {
 
-    var outputArr = { "token": "", "amount": 0, "timestamp": 0 };
+        var output = [];
 
-    var lineReader = require('readline').createInterface({
-        input: require('fs').createReadStream('transactions.csv')
-    });
+        var btcArr = { "token": "BTC", "amount": 0, "timestamp": 0 };
+        var ethArr = { "token": "ETH", "amount": 0, "timestamp": 0 };
+        var xrpArr = { "token": "XRP", "amount": 0, "timestamp": 0 };
 
-    lineReader.on('line', (line) => {
+        var lineReader = require('readline').createInterface({
+            input: require('fs').createReadStream('transactions.csv')
+        });
 
-            var lineObject = {};
+        lineReader.on('line', (line) => {
+
+            // console.log("line ",line); 25/10/2019
+
+                var jsonFromLine = readLine(line);
+                var timeStampToDate = dateFromTimestamp(jsonFromLine);
+                // console.log("timeStampToDate.localeCompare(date) ",timeStampToDate.localeCompare(date));
+
+                if (jsonFromLine.token === 'BTC' && (timeStampToDate.localeCompare(date) == 0)) {
+                    if(jsonFromLine.transaction_type=="DEPOSIT"){
+                        // console.log("btcArr.amount ",btcArr.amount);
+                        btcArr.amount += parseFloat(jsonFromLine.amount);
+                    } else {
+                        // console.log("btcArr.amount ",btcArr.amount);
+                        btcArr.amount -= parseFloat(jsonFromLine.amount);
+                    }
+                    btcArr.timestamp = date;
+
+                } else if (jsonFromLine.token === 'ETH' && (timeStampToDate.localeCompare(date) == 0)) {
+                    if(jsonFromLine.transaction_type=="DEPOSIT"){
+                        ethArr.amount += parseFloat(jsonFromLine.amount);
+                    } else {
+                        ethArr.amount -= parseFloat(jsonFromLine.amount);
+                    }
+                    // console.log("ethArr.amount ",ethArr.amount);
+                    ethArr.timestamp = date;
+
+                } else if (jsonFromLine.token === 'XRP' && (timeStampToDate.localeCompare(date) == 0)) {
+                    if(jsonFromLine.transaction_type=="DEPOSIT"){
+                        xrpArr.amount += parseFloat(jsonFromLine.amount);
+                    } else {
+                        xrpArr.amount -= parseFloat(jsonFromLine.amount);
+                    }
+                    // console.log("xrpArr.amount ",xrpArr.amount);
+                    xrpArr.timestamp = date;
+                }
+            }
+
+        );
+        lineReader.on('close', () => {
+
+            output.push(ethArr);
+            output.push(btcArr);
+            output.push(xrpArr);
+
+            // console.log("output ",output);
+
+            cryptoCompare = getUSDValues();
+
+            cryptoCompare.then(function (result) {
+                usdValues = result;
+
+                output.forEach(element => {
+                    element.amount = element.amount * usdValues.ETH.USD;
+                });
+
+                resolve(output);
+            }, function (err) {
+                console.log(err);
+            });
+        });
+
+        function readLine(line) {
+            var jsonFromLine = {};
             var lineSplit = line.split(',');
 
-            // console.log(lineSplit);
-
-            lineObject.timestamp = lineSplit[0];
-            lineObject.transaction_type = lineSplit[1];
-            lineObject.token = lineSplit[2];
-            lineObject.amount = lineSplit[3];
-
-            if (lineObject.token === 'ETH') {
-                if (lineObject.timestamp > outputArr.timestamp) {
-                    outputArr.token = lineObject.token;
-                    outputArr.amount = lineObject.amount;
-                    outputArr.timestamp = lineObject.timestamp;
-                }
-            }
-            else if (lineObject.token === 'BTC') {
-                if (lineObject.timestamp > outputArr.timestamp) {
-                    outputArr.token = lineObject.token;
-                    outputArr.amount = lineObject.amount;
-                    outputArr.timestamp = lineObject.timestamp;
-
-                }
-            }
-            else if (lineObject.token === 'XRP') {
-                if (lineObject.timestamp > outputArr.timestamp) {
-                    outputArr.token = lineObject.token;
-                    outputArr.amount = lineObject.amount;
-                    outputArr.timestamp = lineObject.timestamp;
-                }
-            }
+            jsonFromLine.timestamp = lineSplit[0];
+            jsonFromLine.transaction_type = lineSplit[1];
+            jsonFromLine.token = lineSplit[2];
+            jsonFromLine.amount = lineSplit[3];
+            return jsonFromLine;
         }
 
-    );
-    lineReader.on('close', (line) => {
-            console.log(outputArr)
-        });
+    });
+}
+
+function dateFromTimestamp(jsonFromLine) {
+    var d = new Date(jsonFromLine.timestamp * 1000);
+    var dateFromCSV = d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
+    return dateFromCSV;
+}
+
+async function getUSDValues() {
+    var cryptoURL = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH&tsyms=BTC,USD,EUR&api_key=3789ea397be622354552b3ab2a826e4379b5da952de997d3cff964ed4f0786ee';
+    const response = await axios.get(cryptoURL);
+    return Promise.resolve(response.data);
+}
+
+if (args.date != undefined && args.token === undefined){
+    console.log("Return the portfolio value per token on a date");
+    getPortfolioValPerToken(args.date).then((result) => {
+        console.log(result);
+    });
+    
+}
