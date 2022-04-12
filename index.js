eedabe72fc72f7e9697c79a9c8db934da029e287
ahsan <zaman2ahsan@gmail.com>
2022-04-12 20:15:33 +0600

    simple fs validation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const args = require('yargs').argv;
 const axios = require('axios');
 const readLine = require('readline');
+const fs = require('fs');
+const filepath = require('path'); 
 
 // Given a token
 // Given a date and a token
@@ -8,48 +10,52 @@ var getPortfolioValForAToken = (token,date) => new Promise(function (resolve) {
 
     var outputArr = { "token": "", "portfolioValue": 0, "timestamp": 0 };
     var lineReader = csvLineReader();
+    
+    if(lineReader != null){
+        lineReader.on('line', function (line) {
 
-    lineReader.on('line', function (line) {
-
-        var jsonFromLine = readLineCSV(line);
-
-        if(date != undefined){
-            var timeStampToDate = dateFromTimestamp(jsonFromLine);
-            // Given a date and a token
-            if (jsonFromLine.token === token && (timeStampToDate.localeCompare(date) == 0)) {
-                outputArr.token = jsonFromLine.token;
-                if(jsonFromLine.transaction_type=="DEPOSIT"){
-                    outputArr.portfolioValue += parseFloat(jsonFromLine.amount);
-                } else {
-                    outputArr.portfolioValue -= parseFloat(jsonFromLine.amount);
-                }
-                outputArr.timestamp = date;
-            }
-        } else{
-            // Given a token
-            if (jsonFromLine.token === token) {
+            var jsonFromLine = readLineCSV(line);
+    
+            if(date != undefined){
+                var timeStampToDate = dateFromTimestamp(jsonFromLine);
+                // Given a date and a token
+                if (jsonFromLine.token === token && (timeStampToDate.localeCompare(date) == 0)) {
                     outputArr.token = jsonFromLine.token;
                     if(jsonFromLine.transaction_type=="DEPOSIT"){
                         outputArr.portfolioValue += parseFloat(jsonFromLine.amount);
                     } else {
                         outputArr.portfolioValue -= parseFloat(jsonFromLine.amount);
                     }
-                    outputArr.timestamp = 'latest';
+                    outputArr.timestamp = date;
+                }
+            } else{
+                // Given a token
+                if (jsonFromLine.token === token) {
+                        outputArr.token = jsonFromLine.token;
+                        if(jsonFromLine.transaction_type=="DEPOSIT"){
+                            outputArr.portfolioValue += parseFloat(jsonFromLine.amount);
+                        } else {
+                            outputArr.portfolioValue -= parseFloat(jsonFromLine.amount);
+                        }
+                        outputArr.timestamp = 'latest';
+                }
             }
-        }
-    });
-
-    lineReader.on('close', function () {
-        cryptoCompare = getUSDValues();
-        cryptoCompare.then(function (result) {
-            usdValues = result;
-            outputArr.portfolioValue = outputArr.portfolioValue * usdValues.ETH.USD;
-            resolve(outputArr);
-        }, function (err) {
-            console.log(err);
         });
-
-    });
+    
+        lineReader.on('close', function () {
+            cryptoCompare = getUSDValues();
+            cryptoCompare.then(function (result) {
+                usdValues = result;
+                outputArr.portfolioValue = outputArr.portfolioValue * usdValues.ETH.USD;
+                resolve(outputArr);
+            }, function (err) {
+                console.log(err);
+            });
+    
+        });
+    }else {
+        console.log("\nSorry!! Can't get result. 'transactions.csv' file doesn't exist in current directory!");
+    }
 })
 
 // Given no parameters
@@ -64,42 +70,46 @@ var getPortfolioValPerTokenInUSD = (token,date) => new Promise(function (resolve
 
     var lineReader = csvLineReader();
 
-    lineReader.on('line', (line) => {
+    if(lineReader != null){
+        lineReader.on('line', (line) => {
 
-        var jsonFromLine = readLineCSV(line);
-
-        if(token === undefined && date === undefined){
-            //Given no parameters
-            setValuePerToken(jsonFromLine, ethArr, btcArr, xrpArr);
-        } else {
-            //Given a date
-            var timeStampToDate = dateFromTimestamp(jsonFromLine);
-            setValuePerTokenForDate(jsonFromLine, timeStampToDate, date, btcArr, ethArr, xrpArr);
+            var jsonFromLine = readLineCSV(line);
+    
+            if(token === undefined && date === undefined){
+                //Given no parameters
+                setValuePerToken(jsonFromLine, ethArr, btcArr, xrpArr);
+            } else {
+                //Given a date
+                var timeStampToDate = dateFromTimestamp(jsonFromLine);
+                setValuePerTokenForDate(jsonFromLine, timeStampToDate, date, btcArr, ethArr, xrpArr);
+            }
         }
-    }
-
-    );
-    lineReader.on('close', (line) => {
-
-        output.push(ethArr);
-        output.push(btcArr);
-        output.push(xrpArr);
-
-        cryptoCompare = getUSDValues();
-
-        cryptoCompare.then(function (result) {
-            usdValues = result;
-
-            output.forEach(element => {
-                element.portfolioValue = element.portfolioValue * usdValues.ETH.USD;
+    
+        );
+        lineReader.on('close', (line) => {
+    
+            output.push(ethArr);
+            output.push(btcArr);
+            output.push(xrpArr);
+    
+            cryptoCompare = getUSDValues();
+    
+            cryptoCompare.then(function (result) {
+                usdValues = result;
+    
+                output.forEach(element => {
+                    element.portfolioValue = element.portfolioValue * usdValues.ETH.USD;
+                });
+    
+                resolve(output);
+            }, function (err) {
+                console.log(err);
             });
-
-            resolve(output);
-        }, function (err) {
-            console.log(err);
+    
         });
-
-        });
+    }else {
+        console.log("\nSorry!! Can't get result. 'transactions.csv' file doesn't exist in current directory!");
+    }
 })
 
 
@@ -168,9 +178,18 @@ function splitCSVcolumns(jsonFromLine, lineSplit) {
 }
 
 function csvLineReader() {
-    return readLine.createInterface({
-        input: require('fs').createReadStream('transactions.csv')
-    });
+    let fileName = 'transactions.csv';
+    try {
+        if (fs.existsSync(fileName)) {
+            return readLine.createInterface({
+                input: fs.createReadStream(fileName)
+            });
+        } else {
+            return null;
+        }
+      } catch(err) {
+        console.error(err);
+      }
 }
 
 function readLineCSV(line) {
@@ -195,26 +214,26 @@ async function getUSDValues() {
 
 
 if(args.token === undefined && args.date === undefined){
-    console.log(`The latest portfolio value per token in USD is here:`);
+    console.log(`The latest portfolio value per token in USD is :`);
     getPortfolioValPerTokenInUSD(args.token,args.date).then((result) => { 
         console.log(result); 
     });
 }
 else if (args.date != undefined && args.token === undefined){
-    console.log(`The portfolio value per token in USD on ${args.date} is here:`);
+    console.log(`The portfolio value per token in USD on ${args.date} is :`);
     getPortfolioValPerTokenInUSD(args.token,args.date).then((result) => {
         console.log(result);
     });
     
 }
 else if (args.token != undefined && args.date === undefined){
-    console.log(`The latest portfolio value for ${args.token} in USD is here:`);
+    console.log(`The latest portfolio value for ${args.token} in USD is :`);
     getPortfolioValForAToken(args.token,args.date).then((result) => {
         console.log(result);
         });
 }
 else if (args.token != undefined && args.date != undefined){
-    console.log(`The portfolio value of ${args.token} in USD on ${args.date} is here:`);
+    console.log(`The portfolio value of ${args.token} in USD on ${args.date} is :`);
     getPortfolioValForAToken(args.token,args.date).then((result) => {
         console.log(result);
     });
